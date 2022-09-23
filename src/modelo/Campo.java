e074d7d582e8f1995d05432f06d912c63fcf8ad0
Natália <48138054+nati-br@users.noreply.github.com>
2022-09-23 03:52:06 -0300

    iniciando a classe campo

--- src/modelo/Campo.java
+++ src/modelo/Campo.java
@@ -1,4 +1,20 @@
 package modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Campo {
+    private final int linha;
+    private final int coluna;
+
+    //valor padrão do boolean é false
+    private boolean aberto;
+    private boolean minado;
+    private boolean marcado;
+    private List<Campo> vizinhos = new ArrayList<>();
+    Campo(int linha, int coluna){
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
 }
